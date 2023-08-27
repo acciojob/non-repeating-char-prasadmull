f490f1dd441fd1cc8cfbb2eae96bfd49f0108f61
Prasad Mullamuri <119662453+prasadmull@users.noreply.github.com>
2023-08-27 18:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 //your JS code here. If required.
-let str=prompt("Enter a string:");
+let str = prompt("Enter a string : ");
 console.log(str);
-let obj={};
-for(let char of str){
+let obj = {};
+for (let char of str) {
 	if(obj[char]) obj[char]++;
-	else obj[char]=1;
+	else{
+		obj[char] = 1;
+	}
 }
 console.log(obj);
-let ans="";
-for(let key in obj){
-	if(obj[key]===1]) ans+=key;
+let ans = "";
+for (let key in obj) {
+	if(obj[key] === 1) ans += key;
 }
-if(ans==="")ans=`' '`;
+if(ans === "") ans = `' '`;
 alert(ans);
