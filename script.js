ab3e75efbb5752a024040fccda98ae8ed8306e0f
Prasad Mullamuri <119662453+prasadmull@users.noreply.github.com>
2023-08-27 18:38:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ for(let char of str){
 	else obj[char]=1;
 }
 console.log(obj);
-let ans=" ";
+let ans="";
 for(let key in obj){
-	if(obj[key===1]) ans+=key;
+	if(obj[key]===1]) ans+=key;
 }
 if(ans==="")ans+=`' '`;
 alert(ans);
